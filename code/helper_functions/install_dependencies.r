c2984f67b3170810480f25393e4e6492eae62654
Leonid Zhizhin <lazhizhin@yandex.ru>
2022-06-24 22:17:23 +0300

    Added reinstallation of stargazer to install_dependencies.r

--- code/helper_functions/install_dependencies.r
+++ code/helper_functions/install_dependencies.r
@@ -1,10 +1,5 @@
-# Install the required dependencies
-
-# Declare working directory beforehand in an environment variable
-# CRIMECOST_PATH = "path_to_your_folder"
-# with the aid of usethis::edit_r_environ()
-# Restart R session for the changes to make effect
-setwd(Sys.getenv('CRIMECOST_PATH'))
+# Get project path
+setwd(Sys.getenv("CRIMECOST_PATH"))
 
 # Read in the file with dependencies
 packages <- readLines(con <- file("DEPENDENCIES", encoding = "UTF-8"))
@@ -13,10 +8,26 @@ packages <- gsub("\\s+.*$", "", packages)
 
 # Install the uninstalled packages
 missing_packages <- setdiff(packages, rownames(installed.packages()))
-
-if ( length(missing_packages) > 0 ) {
-
-	# Install the latest versions of all the packages
-	install.packages(missing_packages, quiet = T) 
-
+if (length(missing_packages) > 0) {
+    # Install the latest versions of all the packages
+    install.packages(missing_packages, quiet = TRUE)
 }
+
+
+## In R >=4.2.0, stargazer <= 5.2.3 returns an error for objects with multiple models with long names.
+## Below is a quick fix for this.
+# Delete stargazer
+remove.packages("stargazer")
+# Download the source
+download.file("https://cran.r-project.org/src/contrib/stargazer_5.2.3.tar.gz", destfile = "stargazer_5.2.3.tar.gz")
+# Unpack
+untar("stargazer_5.2.3.tar.gz")
+# Read the sourcefile with .inside.bracket fun
+stargazer_src <- readLines("stargazer/R/stargazer-internal.R")
+# Move the length check 5 lines up so it precedes is.na(.)
+stargazer_src[1990] <- stargazer_src[1995]
+stargazer_src[1995] <- ""
+# Save back
+writeLines(stargazer_src, con = "stargazer/R/stargazer-internal.R")
+# Compile and install the patched package
+install.packages("stargazer", repos = NULL, type = "source")
